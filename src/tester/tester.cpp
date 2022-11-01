5fc434196f561e2347b07664f9f924d18576608c
Guillaume CAMUZEAUX <gucamuze@e1r5p2.clusters.42paris.fr>
2022-11-02 00:59:47 +0100

    multiple improvements to the tester, still not working as intended when it comes to networking

--- src/tester/tester.cpp
+++ src/tester/tester.cpp
@@ -4,11 +4,13 @@
 #include "Command.hpp"
 #include "functionMap.hpp"
 #include <arpa/inet.h>
-
-#define BUFFER_LEN 2048
 #include <fstream>
 
-static void	test_command(int socket_fd, std::string test_str, char *server_reponse);
+#define BUFFER_LEN			1024
+#define TESTER_FILES_PATH	std::string("./tester_files/")
+#define CONFIG_FILE_PATH	"./tester_files/config.test"
+
+static void	test_command(int socket_fd, std::string test_str);
 static void replace_tokens(std::string &str, const std::string &password,
 									const std::string &nick, const std::string &username);
 
@@ -17,25 +19,22 @@ static void launch_tests(int socket_fd, const std::string &password,
 	const std::string &nick, const std::string &username, char **test_files)
 {
 	std::string	msg;
-	char server_response[BUFFER_LEN];
 
 	// Clean buffers:
-	memset(server_response, '\0', BUFFER_LEN);
 	std::string		test_msg;
 
 	for (int i = 0; test_files[i] != NULL; i++) {
-		std::ifstream	infile(test_files[i]);
+		std::string		file = TESTER_FILES_PATH + test_files[i];
+		std::ifstream	infile(file.c_str());
 		while (std::getline(infile, test_msg)) {
 			if (!test_msg.empty() && test_msg.at(0) != '#') {
 				replace_tokens(test_msg, password, nick, username);
 				std::cout << "======== TESTER: Sending command [" << test_msg << "] =========\n"; 
 				test_msg += IRC_MSG_SEPARATOR;
-				test_command(socket_fd, test_msg, server_response);
+				test_command(socket_fd, test_msg);
 			}
 		}
 	}
-	
-
 }
 
 static void replace_tokens(std::string &str, const std::string &password,
@@ -50,9 +49,10 @@ static void replace_tokens(std::string &str, const std::string &password,
 		str.replace(ret, 9, username);
 }
 
-static void	test_command(int socket_fd, std::string test_str, char *server_response)
+static void	test_command(int socket_fd, std::string test_str)
 {
-	// Get input from the user:
+	char	server_response[BUFFER_LEN];
+	memset(server_response, '\0', BUFFER_LEN);
 	test_str += IRC_MSG_SEPARATOR;
 
     // Send the message to server:
@@ -61,29 +61,59 @@ static void	test_command(int socket_fd, std::string test_str, char *server_respo
         return;
 	}
 	
-	usleep(1000);
+	usleep(10000);
     // Receive the server's response:
 	int read_ret = 0;
-	if ((read_ret = read(socket_fd, server_response, BUFFER_LEN)) == -1) {
-        std::cout << "No server response\n";
+	while ((read_ret = read(socket_fd, server_response, BUFFER_LEN)) <= 0) {
+        ;
 	}
-    else
-    	std::cout << "Server's response: " << server_response << std::endl;
+	std::cout << "Server's response: " << server_response << std::endl;
+}
+
+static bool	init_tester_config(int &port, std::string &password,
+	std::string &nick, std::string &username)
+{
+	std::string		rl_str;
+	std::ifstream	infile(CONFIG_FILE_PATH);
+	unsigned int	init_count = 0;
+
+	while (std::getline(infile, rl_str)) {
+		if (!rl_str.empty() && rl_str.at(0) != '#') {
+			if (init_count == 0)
+				port = std::atoi(rl_str.c_str());
+			else if (init_count == 1)
+				password = rl_str;
+			else if (init_count == 2)
+				nick = rl_str;
+			else if (init_count == 3)
+				username = rl_str;
+			init_count++;
+		}
+	}
+	return (init_count == 4 ? true : false);
 }
 
 int main(int ac, char **av)
 {
-	(void)av;
-	if (ac < 5)
-	{
-		std::cout << "Usage: ./irc_tester [port] [password] [nick] [username] {... [test_file(s)]}\n";
+	if (ac < 2) {
+		std::cout << "USAGE: ./tester {-help} ...[test_files]\n";
 		return (EXIT_FAILURE);
 	}
 
-	const int			port = std::atoi(av[1]);
-	const std::string	password = av[2];
-	const std::string	nick = av[3];
-	const std::string	username = av[4];
+	int	port;
+	std::string	password, nick, username;
+
+	if (std::strncmp(av[1], "-help", 6) == 0) {
+		std::cout << "USAGE: ./tester {-help} ...[test_files]\n"
+			<< "Config file is ./tester_files/config.test\n"
+			<< "Only the name of the test file is needed, this tester will "
+			<< "automatically preprend './tester_files/' to each file given as argument\n";
+	}
+
+	if (init_tester_config(port, password, nick, username) == false) {
+		std::cout << "Config error\n";
+		return (EXIT_FAILURE);
+	}
 
 	int					socket_fd;
 	struct sockaddr_in	server_addr;
@@ -118,7 +148,7 @@ int main(int ac, char **av)
 		std::cerr << e.what() << '\n';
 	}
 
-	launch_tests(socket_fd, password, nick, username, &av[5]);
+	launch_tests(socket_fd, password, nick, username, av);
 		// Close the socket:
 	close(socket_fd);
 }
