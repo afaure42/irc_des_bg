29ba06679514523bfc2fddfa3fd81f4ecce7c70b
afaure <afaure@student.42.fr>
2022-11-17 16:38:56 +0100

    added sendAllUser function to send a msg to every user on the server

--- includes/execCommandUtils.hpp
+++ includes/execCommandUtils.hpp
@@ -11,6 +11,17 @@ typedef std::map<unsigned int, User>	t_users;
 // channels map
 typedef std::vector<Channel> 			t_channels;
 
+/**
+ * @brief Function to send a msg to every user on the server
+ * 
+ * @param scheduler reference to the scheduler
+ * @param msg msg to send
+ * @param from user sending the message
+ * 	( he will not receive the msg )
+ * 	0 if you dont care
+ */
+void sendAllUser(Scheduler & scheduler, t_users & users,
+	const std::string & msg, unsigned int from);
 t_channels::iterator	findChannel(const std::string & name, t_channels & channels);
 t_users::iterator		findUser(const std::string & nick, t_users & users);
 void					freeUser(unsigned int client_id, Server & server,
