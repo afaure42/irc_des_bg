5c61f145ad6257e80ef684b4af67cb48ddd07d99
Guillaume CAMUZEAUX <gucamuze@e1r1p3.clusters.42paris.fr>
2022-11-20 13:28:00 +0100

    tester done

--- src/tester/tester.cpp
+++ src/tester/tester.cpp
@@ -1,49 +1,44 @@
 #include "../../includes/tester.hpp"
 
-// static void launch_tests(int socket_fd, const std::string &password,
-// 	const std::string &nick, const std::string &username, const std::string &fullname, char **test_files)
-// {
-// 	std::string	msg;
+static void launch_tests(struct tester_client_s tc, char **test_files)
+{
+	std::string	msg;
 
-// 	// Clean buffers:
-// 	std::string		test_msg;
+	// Clean buffers:
+	std::string		test_msg;
 
-// 	for (int i = 1; test_files[i] != NULL; i++) {
-// 		std::string		file = test_files[i];
-// 		std::ifstream	infile(file.c_str());
-// 		if (infile.is_open()) {
-// 			while (std::getline(infile, test_msg)) {
-// 				if (!test_msg.empty() && test_msg.at(0) != '#') {
-// 					if (*(test_msg.end() - 1) == '\n')
-// 						test_msg.erase(test_msg.size() - 1); //remove trailing \n
-// 					replace_tokens(test_msg, password, nick, username, fullname);
-// 					std::cout << "======== TESTER: Sending command [" << test_msg << "] =========\n"; 
-// 					test_msg += IRC_MSG_SEPARATOR;
-// 					test_command(socket_fd, test_msg);
-// 				}
-// 			}
-// 		} else {
-// 			std::cout << "Invalid test file " << file << std::endl;
-// 		}
-// 	}
-// }
+	for (int i = 1; test_files[i] != NULL; i++) {
+		std::string		file = test_files[i];
+		std::ifstream	infile(file.c_str());
+		if (infile.is_open()) {
+			while (std::getline(infile, test_msg)) {
+				if (!test_msg.empty() && test_msg.at(0) != '#') {
+					if (*(test_msg.end() - 1) == '\n')
+						test_msg.erase(test_msg.size() - 1); //remove trailing \n
+					replace_tokens(test_msg, tc.tci);
+					std::cout << "======== TESTER: Sending command [" << test_msg << "] =========\n"; 
+					// test_msg += IRC_MSG_SEPARATOR;
+					send_command(tc.socket_fd, test_msg);
+				}
+			}
+		} else {
+			std::cout << "Invalid test file " << file << std::endl;
+		}
+	}
+}
 
-// static void replace_tokens(std::string &str,
-// 							const std::string &password,
-// 							const std::string &nick,
-// 							const std::string &username,
-// 							const std::string &fullname)
-// {
-// 	size_t ret;
-// 	while ((ret = str.find("$PASSWORD")) != str.npos)
-// 		str.replace(ret, 9, password);
-// 	while ((ret = str.find("$NICK")) != str.npos)
-// 		str.replace(ret, 5, nick);
-// 	while ((ret = str.find("$USERNAME")) != str.npos)
-// 		str.replace(ret, 9, username);
-// 	while ((ret = str.find("$FULLNAME")) != str.npos)
-// 		str.replace(ret, 9, fullname);
-// }
+static void replace_tokens(std::string &str, struct tester_client_info_s &tci)
+{
+	size_t ret;
+	while ((ret = str.find("$PASSWORD")) != str.npos)
+		str.replace(ret, 9, tci.password);
+	while ((ret = str.find("$NICK")) != str.npos)
+		str.replace(ret, 5, tci.nick);
+	while ((ret = str.find("$USERNAME")) != str.npos)
+		str.replace(ret, 9, tci.username);
+	while ((ret = str.find("$FULLNAME")) != str.npos)
+		str.replace(ret, 9, tci.fullname);
+}
 
 static void	send_command(int socket_fd, std::string command)
 {
@@ -57,7 +52,7 @@ static void	send_command(int socket_fd, std::string command)
         return;
 	}
 	
-	// usleep(100000);
+	usleep(100000);
     // Receive the server's response:
 	int read_ret = 0;
 	while ((read_ret = read(socket_fd, server_response, BUFFER_LEN)) >= 0) {
@@ -90,20 +85,23 @@ static bool	init_tester_config(struct tester_client_info_s &tci)
 	return (init_count == 5);
 }
 
-struct tester_client_s	init_client(size_t i, client_list_t, struct tester_client_info_s tci)
+struct tester_client_s	init_client(int i, struct tester_client_info_s tci)
 {
 	struct tester_client_s	tc;
 
 	// init client info
 	tc.tci = tci;
-	std::string	client_name = "test";
-	while (i > 0) {
-		client_name += (i % 10) + 48;
-		i /= 10;
+	// modify names to be test + index for stress test
+	if (i >= 0) {
+		std::string	client_name = "test";
+		while (i > 0) {
+			client_name += (i % 10) + 48;
+			i /= 10;
+		}
+		tc.tci.nick = client_name;
+		tc.tci.fullname = "FN"+client_name;
+		tc.tci.username = "U"+client_name;
 	}
-	tc.tci.nick = client_name;
-	tc.tci.fullname = "FN"+client_name;
-	tc.tci.username = "U"+client_name;
 
 	// init client socket
 	try
@@ -166,11 +164,12 @@ int main(int ac, char **av)
 		return (EXIT_FAILURE);
 	}
 
+	// Stress test mode
 	if (std::strncmp(av[1], "-st", 3) == false) {
 		client_list_t	client_list;
 		// create clients
 		for (size_t i = 0; i < STRESSTEST_CLIENTS; i++)
-			client_list.push_back(init_client(i, client_list, tci));\
+			client_list.push_back(init_client(i, tci));
 		// make them login
 		for (client_list_t::iterator it = client_list.begin(); it != client_list.end(); it++)
 			login_client(*it);
@@ -188,42 +187,10 @@ int main(int ac, char **av)
 			close(it->socket_fd);
 		}
 	}
-
-	// int					socket_fd;
-	// struct sockaddr_in	server_addr;
-
-	// try
-	// {
-	// 	// Create socket:
-	// 	socket_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-
-	// 	if (socket_fd < 0)
-	// 	{
-	// 		std::cout << "Unable to create socket\n";
-	// 		return -1;
-	// 	}
-
-	// 	std::cout << "Socket created successfully\n";
-
-	// 	// Set port and IP the same as server-side:
-	// 	server_addr.sin_family		= AF_INET;
-	// 	server_addr.sin_port		= htons(port);
-	// 	server_addr.sin_addr.s_addr	= inet_addr("127.0.0.1");
-
-	// 	// Send connection request to server:
-	// 	while (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-	// 	{
-	// 		std::cout << "Unable to connect\n";
-	// 	}
-	// 	std::cout << "Connected with server successfully\n";
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
-
-	// launch_tests(socket_fd, password, nick, username, fullname, av);
-		// Close the socket:
-	// std::cout << "Tests end, closing server connexion\n";
-	// close(socket_fd);
+	// Manual tests mode
+	else {
+		struct tester_client_s	tc = init_client(-1, tci);
+		launch_tests(tc, av);
+		close(tc.socket_fd);
+	}
 }
